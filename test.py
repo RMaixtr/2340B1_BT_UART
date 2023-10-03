da1c7cec1b65757ec846bccf5e94bc41865eb087
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-10-04 00:06:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,9 @@ if __name__ == '__main__':
     blu = e104_bt08.e104_bt08(baudrate=115200, bytesize=8, stopbits=1, timeout=1,
                               datacallback=bt08_data_callback, statecallback=bt08_state_callback)
     while True:
-        user_input = input("按下 Enter 键来结束程序：")
+        user_input = input("按下 Enter 键来结束程序,按其他键发送信息")
         if user_input == "":
             break
+        if blu.get_state() == e104_bt08.AT_STATE_CONNECT:
+            blu.write("test")
     blu.close()
