9f9294133779c396246dc98a675a114b4fdac81d
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-10-03 16:33:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,14 +11,8 @@ def bt08_state_callback(self, data):
 
 
 if __name__ == '__main__':
-    blu = e104_bt08.e104_bt08(baudrate=115200, bytesize=8, stopbits=1, timeout=1)
-    blu.start()
-    blu.set_data_callback(bt08_data_callback)
-    blu.set_state_callback(bt08_state_callback)
-    try:
-        blu.enter_at()
-    except Exception:
-        blu.reset()
+    blu = e104_bt08.e104_bt08(baudrate=115200, bytesize=8, stopbits=1, timeout=1,
+                              datacallback=bt08_data_callback, statecallback=bt08_state_callback)
     while True:
         user_input = input("按下 Enter 键来结束程序：")
         if user_input == "":
