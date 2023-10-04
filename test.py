75d6c1ae96d34d4d6d094d888a78e5d05d9dfb5f
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-10-04 16:32:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,8 +11,7 @@ def bt08_state_callback(self, data):
 
 
 if __name__ == '__main__':
-    blu = e104_bt08.e104_bt08(baudrate=115200, bytesize=8, stopbits=1, timeout=1,
-                              datacallback=bt08_data_callback, statecallback=bt08_state_callback)
+    blu = e104_bt08.e104_bt08(datacallback=bt08_data_callback, statecallback=bt08_state_callback)
     while True:
         user_input = input("按下 Enter 键来结束程序,按其他键发送信息")
         if user_input == "":
