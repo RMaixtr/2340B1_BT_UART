3ffd635ec0b70e9b15f6984fbb7dbe90c6eee4a4
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-10-10 19:38:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,25 +13,39 @@ def bt08_state_callback(self, data):
 
 
 def unit_test_loop_change_id():
-    while not e104_bt08.isatmode:
-        time.sleep(1)
     for index in range(10):
-        e104_bt08.set_name(str(index).encode())
-        e104_bt08.reset()
+        try:
+            e104_bt08.set_name(str(index).encode())
+            e104_bt08.reset()
+        except Exception as e:
+            if str(e) == bt.AT_ERR[b'4']:
+                e104_bt08.set_name(str(index).encode())
+                e104_bt08.reset()
+            elif str(e) == "等待AT指令回应超时" or str(e) == "等待上次AT指令回应超时":
+                try:
+                    e104_bt08.enter_at()
+                except Exception as e:
+                    if str(e) == "等待AT指令回应超时" or str(e) == "等待上次AT指令回应超时":
+                        print("请检查蓝牙模块是否断开")
+                        exit()
+                e104_bt08.set_name(str(index).encode())
+                e104_bt08.reset()
+
         time.sleep(1)
 
 
 def unit_test_loop_write_read():
-    while e104_bt08.get_state() != bt.AT_STATE_CONNECT:
-        time.sleep(1)
     for index in range(10):
-        e104_bt08.write(str(index).encode())
-        time.sleep(1)
+        if e104_bt08.is_connected():
+            e104_bt08.write(str(index).encode())
+            time.sleep(1)
+            print('read', e104_bt08.read())
 
 
 if __name__ == '__main__':
     e104_bt08.add_state_callback(bt08_state_callback)
     e104_bt08.add_data_callback(bt08_data_callback)
     unit_test_loop_change_id()
+    input("请连接蓝牙模块")
     unit_test_loop_write_read()
     e104_bt08.close()
