cabb32ad80f3dce83c6f1e0f5406fac4c21db171
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-10-26 17:27:52 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,12 @@
-import time
+a = 1
+
 while True:
-    print(time.asctime())
+    hello_img = image.new(size = (240, 240),
+                                  color = (255, 0, 0), mode = "RGB")
+    hello_img.draw_string(30, 115, str(a), scale = 10.0,
+                                  color = (255, 255, 255), thickness = 1)
+    display.show(hello_img)
+    print(str(a))
+    a = a+1
     time.sleep(1)
+
