e97d75b466b258a712d61930f5e2f9cf36d1a8f6
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-10-31 07:58:00 +0800

    Update test_send_file_host.py

--- test_send_file_host.py
+++ test_send_file_host.py
@@ -1,7 +1,16 @@
 import time
 from bt import e104_bt08
 
+def bt08_data_callback(self, data):
+    print("datacallback", data)
+
+
+def bt08_state_callback(self, data):
+    print("statechange", data)
+
 if __name__ == '__main__':
+    e104_bt08.add_state_callback(bt08_state_callback)
+    e104_bt08.add_data_callback(bt08_data_callback)
     # 主机要发送给从机的程序 example.py
     with open('example .py', 'rb') as f:
         example = f.read()
