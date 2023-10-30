3bcff403289bedc116770dc39724c871ef63b8f6
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-10-30 20:25:47 +0800

    Update test_send_file_host.py

--- test_send_file_host.py
+++ test_send_file_host.py
@@ -1,26 +1,28 @@
 import time
 from bt import e104_bt08
 
-
-def bt08_data_callback(self, data):
-    # self.write(data)
-    print("datacallback", data)
-
-
-def bt08_state_callback(self, data):
-    print("statechange", data)
-
-
 if __name__ == '__main__':
-    e104_bt08.init()
-    e104_bt08.add_state_callback(bt08_state_callback)
-    e104_bt08.add_data_callback(bt08_data_callback)
-    input("连接蓝牙后按回车")
-    e104_bt08.send_file(b'example.py', b'example')
-    while e104_bt08.sendflag:
+    # 主机要发送给从机的程序 example.py
+    with open('example .py', 'rb') as f:
+        example = f.read()
+    print(example)
+
+    state = 0
+    start_time = time.time()
+    while True:
+        if not e104_bt08.is_connected():
+            state = 0
+        elif state == 0:
+            if e104_bt08.is_connected():
+                e104_bt08.send_file(b'example.py')
+                state = 1
+        elif state == 1:
+            if e104_bt08.sendflag:
+                e104_bt08.slave_run()
+                state = 2
+                start_time = time.time()
+        elif state == 2:
+            if time.time() - start_time >= 5:  # 运行5秒后停止运行
+                e104_bt08.slave_stop()
+                break
         time.sleep(1)
-    time.sleep(1)
-    e104_bt08.slave_run(b'example')
-    time.sleep(5)
-    e104_bt08.slave_stop()
-    e104_bt08.close()
