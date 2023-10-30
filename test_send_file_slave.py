5e1c9eec7f6de226bde046cee3f6e30bfd41375b
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-10-30 20:26:16 +0800

    Update test_send_file_slave.py

--- test_send_file_slave.py
+++ test_send_file_slave.py
@@ -2,5 +2,5 @@ from maix import camera, display, image
 from bt import e104_bt08
 
 if __name__ == '__main__':
-    e104_bt08.init(_globals=globals())
-    input()
+    e104_bt08.set_globals(globals())  # 于主代码中导入运行文本文件所需的模块需调用set_globals传入globals,或于bt.py导入模块处导入
+    input("wait host run code...")
