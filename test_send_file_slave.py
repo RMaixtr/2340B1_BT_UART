9917db6e20cb2fc8b03a549b0e910b1c740bff26
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-10-30 20:30:37 +0800

    Update test_send_file_slave.py

--- test_send_file_slave.py
+++ test_send_file_slave.py
@@ -3,4 +3,10 @@ from bt import e104_bt08
 
 if __name__ == '__main__':
     e104_bt08.set_globals(globals())  # 于主代码中导入运行文本文件所需的模块需调用set_globals传入globals,或于bt.py导入模块处导入
+    hello_img = image.new(size = (240, 240),
+                                  color = (255, 0, 0), mode = "RGB")
+    hello_img.draw_string(30, 115, "wait", scale = 3,
+                                  color = (255, 255, 255), thickness = 1)
+    display.show(hello_img)
+    e104_bt08.init(_globals=globals())
     input("wait host run code...")
