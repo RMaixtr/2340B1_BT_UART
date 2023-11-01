0843184c0c563ad46ff44e957581e87107c5d67a
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-11-01 23:02:00 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
+# from maix import camera, display, image 此处内容用于测试中文传输
+# 在模块中导入运行文本文件所需的模块,或于主代码处导入模块,使用set_globals导入主代码globals
 a = 1
-
 while True:
     hello_img = image.new(size = (240, 240),
                                   color = (255, 0, 0), mode = "RGB")
@@ -9,4 +10,3 @@ while True:
     print(str(a))
     a = a+1
     time.sleep(1)
-
