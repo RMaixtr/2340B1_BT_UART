207b6257a8cabd981497d6bd9dd1b54680613d58
RMaixtr <87690777+RMaixtr@users.noreply.github.com>
2023-12-05 10:36:30 +0800

    Update bt.py

--- bt.py
+++ bt.py
@@ -204,7 +204,7 @@ class E104_BT08(threading.Thread):
                                 self.write(b'\xff\xff' + data[-8:])
                             elif self.getlen > len(self.getdata) and self.getlen > 0:
                                 redata = b'\xff\xff' + hex(len(self.getdata))[2:].zfill(6).encode() \
-                                         + hex(crc8(self.getdata[self.getlen-1]))[2:].zfill(2).encode()
+                                         + hex(crc8(self.getdata[len(self.getdata)-1]))[2:].zfill(2).encode()
 
                                 self.write(redata)
                             else:
